fbe653148a21877f1dfa66791150afa1ecfd06ea
Abhinav Mishra <abhimishra142@gmail.com>
2025-03-31 17:39:26 +0530

    Single and comma seperated number sum

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe 'String Calculator' do
   it 'returns 0 for an empty string' do
     expect(calculator.add('')).to eq(0)
   end
+
+  it 'It should return self number for a single number' do
+    calculator = StringCalculator.new
+    expect(calculator.add('1')).to eq(1)
+  end
+
+  it 'It should return the sum of two comma separated numbers' do
+    calculator = StringCalculator.new
+    expect(calculator.add('1,5')).to eq(6)
+  end
 end
